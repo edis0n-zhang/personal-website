ae87381c0479e6701c08fcd7687c972344cb6a4f
Edison Zhang <edisonzhang@ucsb.edu>
2023-09-10 11:45:19 -0700

    Quartz sync: Sep 10, 2023, 11:45 AM

---
+++ content/index.md
@@ -0,0 +1,4 @@
+---
+title: Hi I'm Edison!
+---
+I'm a second year computer science major at the University of California, Santa Barbara.
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "LinkedIn": "https://www.linkedin.com/in/edison--zhang/",
+      "GitHub": "https://github.com/edis0n-zhang",
     },
   }),
 }
