c80c5e1bfbbd398de7c20ccad1c9abed43995b49
Edison Zhang <edisonzhang@ucsb.edu>
2023-09-21 07:58:39 -0700

    pagetitle test

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "edison zhang",
+    pageTitle: "edisonzhang",
     enableSPA: true,
     enablePopovers: false,
     analytics: {
